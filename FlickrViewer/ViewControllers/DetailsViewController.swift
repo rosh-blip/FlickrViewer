a012c6ded76b3212fcfab5e1c072c03d4922fe9b
Roshan-Abraham-AHMRNQ <65516789+rosh-blip@users.noreply.github.com>
2023-04-20 15:43:34 +1000

    Standalone Navigation Bar implemented

--- FlickrViewer/ViewControllers/DetailsViewController.swift
+++ FlickrViewer/ViewControllers/DetailsViewController.swift
@@ -12,7 +12,12 @@ final class DetailsViewController: UIViewController {
     private var viewModel: DetailsViewModel?
     private let scrollView = UIScrollView()
     private let contentView = UIView()
-//    private let navBar = UINavigationBar() // plans to implement a standalone navigation bar
+    
+    private let navBar:  UINavigationBar = {
+        let nav = UINavigationBar(frame: .zero)
+        nav.backgroundColor = .systemGray
+        return nav
+    }()
     
     let imageView = UIImageView()
     let titleLabel = FVLabel(fontSize: .heading)
@@ -39,21 +44,43 @@ final class DetailsViewController: UIViewController {
         view.backgroundColor = .systemBackground
         viewModel?.assignData(vc: self)
         
-        configureScrollView()
-        configureCloseButton()
-        configureImageView()
-        configureLabels()
+        configureDetailsUI()
     }
     
+    private func configureDetailsUI(){
+        configureNavBar()
+        configureScrollView()
+        configureImageView()
+        configureLabels()
+        view.bringSubviewToFront(navBar)
+    }
     
-//    private func configureNavBar() {
-//
-//
-//        let navItems = UINavigationItem()
-//        navBar.popItem(animated: true) // probs use this in the did touch inside feature
-//        navItems.rightBarButtonItem = UIBarButtonItem(customView: closeButton)
-//
-//    }
+    private func configureNavBar() {
+
+        let doneButton: UIBarButtonItem = {
+            let button = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(closeButtonTapped))
+            return button
+        }()
+        
+        
+        let navItem: UINavigationItem = {
+            let item = UINavigationItem()
+            item.title = "Details"
+            item.rightBarButtonItem = doneButton
+            return item
+        }()
+        
+        navBar.items = [navItem]
+        
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(navBar)
+        NSLayoutConstraint.activate([
+            navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navBar.widthAnchor.constraint(equalTo: view.widthAnchor),
+            navBar.heightAnchor.constraint(equalToConstant: 44)
+        ])
+        
+    }
     
     private func configureScrollView(){
         scrollView.translatesAutoresizingMaskIntoConstraints = false
@@ -64,8 +91,8 @@ final class DetailsViewController: UIViewController {
         NSLayoutConstraint.activate([
             scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            scrollView.topAnchor.constraint(equalTo: navBar.bottomAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         NSLayoutConstraint.activate([
             contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
@@ -75,25 +102,13 @@ final class DetailsViewController: UIViewController {
         ])
     }
     
-    
-    private func configureCloseButton() {
-        contentView.addSubview(closeButton)
-        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
-            closeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            closeButton.heightAnchor.constraint(equalToConstant: 20),
-            closeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20)
-        ])
-    }
-    
     private func configureImageView() {
         contentView.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            imageView.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: spacing),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: spacing),
             imageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: imageWidth)
         ])
     }
